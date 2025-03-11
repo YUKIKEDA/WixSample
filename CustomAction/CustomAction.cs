1ca06403461e2cf5bb924134a2715103473d5787
YUKIKEDA <yuuuu1008ki@gmail.com>
2025-03-11 12:45:15 +0900

    ADD: カスタムメッセージボックスを追加

--- CustomAction/CustomAction.cs
+++ CustomAction/CustomAction.cs
@@ -1,6 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Windows.Forms;
 using WixToolset.Dtf.WindowsInstaller;
 
 namespace CustomAction
@@ -13,6 +12,22 @@ namespace CustomAction
             session.Log("Begin CustomAction1");
 
             return ActionResult.Success;
+        }
+
+        [CustomAction]
+        public static ActionResult ShowUninstallMessage(Session session)
+        {
+            try
+            {
+                session.Log("Begin ShowUninstallMessage");
+                MessageBox.Show(session["UninstallCompleteMessage"], "情報", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return ActionResult.Success;
+            }
+            catch (Exception ex)
+            {
+                session.Log($"Error in ShowUninstallMessage: {ex.Message}");
+                return ActionResult.Failure;
+            }
         }
     }
 }
